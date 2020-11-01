1198db0ca07a2f3b921aa2486d248b920bd67c0c
ogulyukin <o.gulyukin@yandex.ru>
2020-11-01 15:32:50 +0200

    Изучение классов. Конструкторы

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace  std;
+//#define BASICS
 class Point{
     double x;
     double y;
@@ -16,14 +17,42 @@ public:
     void set_y(double y){
         this->y = y;
     }
+    void print(){
+        cout << "X = " << this->x << "\tY = " << this->y << endl;
+    }
+    //Costructors:
+    Point(){
+        x = y = double();
+        cout << "DefaultConstructor:\t" << this << endl;
+    }
+    Point(double x, double y){
+        this->x  = x;
+        this->y = y;
+        cout << "Constructor with two parametrs:\t" << this << endl;
+    }
+    Point(double x){
+        this->x = x;
+        this->y = double();
+        cout << "Constructor with one parametr:\t" << this << endl;
+    }
+    ~Point(){
+        cout << "Destructor:\t" << this << endl;
+    }
 };
 int main() {
     setlocale(LC_ALL, "");
     Point A;
+    A.print();
+    Point B(2,5);
+    B.print();
+    Point C = 5;
+    C.print();
+#ifdef BASICS
     A.set_x(2);
     A.set_y(5);
     cout << A.get_x() << "\t" << A.get_y() << endl;
     Point* pA = &A;
     cout << pA->get_x() << "\t" <<  pA->get_y() << endl;
+#endif
     return 0;
 }
