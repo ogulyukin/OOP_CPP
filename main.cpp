82d6102e26c0de09baccff24a0faa4b73a7f5cb5
ogulyukin <o.gulyukin@yandex.ru>
2020-11-01 14:10:23 +0200

    Изучение классов. Инкапсуляция

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
-
+using namespace  std;
+class Point{
+    double x;
+    double y;
+public:
+    double get_x() const{
+        return x;
+    }
+    double get_y() const{
+        return y;
+    }
+    void set_x(double x){
+        this->x = x;
+    }
+    void set_y(double y){
+        this->y = y;
+    }
+};
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    setlocale(LC_ALL, "");
+    Point A;
+    A.set_x(2);
+    A.set_y(5);
+    cout << A.get_x() << "\t" << A.get_y() << endl;
+    Point* pA = &A;
+    cout << pA->get_x() << "\t" <<  pA->get_y() << endl;
     return 0;
 }
