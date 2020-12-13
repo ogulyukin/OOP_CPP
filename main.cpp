6b9c2c0100d10e8b8289ddbea555e31442f652af
ogulyukin <o.gulyukin@yandex.ru>
2020-12-13 15:37:47 +0200

    Done!

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ inline Fraction operator/(Fraction left, Fraction right);
 inline Fraction operator-(Fraction left, Fraction right);
 ostream& operator<<(ostream& os, const Fraction& obj);
 bool operator==(const Fraction& left, const Fraction& right);
+//////////////////////////////////////////////////////////////////////////////
+//begin class definition
 class HouseNumber{
     int number;
 public:
@@ -28,150 +30,186 @@ class Fraction{
     int numerator;
     int denomenator;
 public:
-    int get_integer()const{
-        return this->integer;
+    int get_integer()const;
+    int get_numerator()const;
+    int get_denominator()const;
+    void set_integer(int integer);
+    void set_numenator(int numenator);
+    void set_denomenator(int denomenator);
+    void print_fraction()const;
+    void print_adress()const;
+    Fraction& to_proper();
+    void to_improper();
+    Fraction inverted();
+    Fraction& reduce();
+    //Constructors
+    Fraction();
+    Fraction(int integer);
+    Fraction(int numerator, int denumerator);
+    Fraction(int integer , int numerator, int denumerator);
+    Fraction(const Fraction& other);
+    ~Fraction();
+    //Operators:
+    Fraction& operator=(const Fraction& other);
+    Fraction& operator*=(const Fraction& other);
+    Fraction& operator/=(const Fraction other);
+    //increment/decrement
+    Fraction& operator++();
+    Fraction operator++(int);
+    Fraction& operator--();
+    //Type cast operator
+    explicit operator int();
+    explicit operator double();
+    //operator HouseNumber();
+};
+//End of class declaration
+////////////////////////////////////////////////////////////////////////////////
+//Class Definition
+int Fraction::get_integer()const{
+    return this->integer;
+}
+int Fraction::get_numerator()const{
+    return this->numerator;
+}
+int Fraction::get_denominator()const{
+    return this->denomenator;
+}
+void Fraction::set_integer(int integer){
+    this->integer = integer;
+}
+void Fraction::set_numenator(int numenator){
+    this->numerator = numenator;
+}
+void Fraction::set_denomenator(int denomenator){
+    this->denomenator = denomenator ? denomenator : 1;
+}
+void Fraction::print_fraction()const{
+    //cout << this->integer << " " << this->numerator << "/" << this->denomenator << endl;
+    if (integer)cout << integer;
+    if (numerator){
+        if(integer)cout << "(";
+        cout << numerator << "/" << denomenator;
+        if(integer)cout << ")";
     }
-    int get_numerator()const{
-        return this->numerator;
-    }
-    int get_denominator()const{
-        return this->denomenator;
-    }
-    void set_integer(int integer){
-        this->integer = integer;
-    }
-    void set_numenator(int numenator){
-        this->numerator = numenator;
-    }
-    void set_denomenator(int denomenator){
-        this->denomenator = denomenator ? denomenator : 1;
-    }
-    void print_fraction()const{
-        //cout << this->integer << " " << this->numerator << "/" << this->denomenator << endl;
-        if (integer)cout << integer;
-        if (numerator){
-            if(integer)cout << "(";
-            cout << numerator << "/" << denomenator;
-            if(integer)cout << ")";
-        }
-        if(integer == 0 && numerator == 0)cout << 0;
-        cout << endl;
-    }
-    void print_adress()const{
-        cout << "Object:\t\t" << this << endl;
-        cout << "Integer:\t\t" << &integer << endl;
-        cout << "Numenator:\t\t" << &numerator << endl;
-        cout << "Denomerator:\t" << &denomenator << endl;
-    }
-    Fraction& to_proper(){
-        integer += numerator / denomenator;
-        numerator %= denomenator;
+    if(integer == 0 && numerator == 0)cout << 0;
+    cout << endl;
+}
+void Fraction::print_adress()const{
+    cout << "Object:\t\t" << this << endl;
+    cout << "Integer:\t\t" << &integer << endl;
+    cout << "Numenator:\t\t" << &numerator << endl;
+    cout << "Denomerator:\t" << &denomenator << endl;
+}
+Fraction& Fraction::to_proper(){
+    integer += numerator / denomenator;
+    numerator %= denomenator;
+    return *this;
+}
+void Fraction::to_improper(){
+    numerator += integer * denomenator;
+    integer = 0;
+}
+Fraction Fraction::inverted(){
+    this->to_improper();
+    return Fraction(denomenator,numerator);
+}
+Fraction& Fraction::reduce(){
+    if (!numerator){
         return *this;
     }
-    void to_improper(){
-        numerator += integer * denomenator;
-        integer = 0;
-    }
-    Fraction inverted(){
-        this->to_improper();
-        return Fraction(denomenator,numerator);
-    }
-    Fraction& reduce(){
-        if (!numerator){
-            return *this;
-        }
-        int more,less,rest;
-        if(numerator < denomenator){
-            less = numerator;
-            more = denomenator;
-        }else{
-            less = denomenator;
-            more = numerator;
-        }/*
+    int more,less,rest;
+    if(numerator < denomenator){
+        less = numerator;
+        more = denomenator;
+    }else{
+        less = denomenator;
+        more = numerator;
+    }/*
         do{
             rest = more%less;
             more = less;
             less = rest;
         }while(rest);*/
-        for(rest = 1;rest;more = less,less = rest)rest = more%less;
-        int GCD = more; //GCD Greatest Common Dividor(Наибольший общий делитель)
-        numerator /=GCD;
-        denomenator /=GCD;
-        return *this;
-    }
-    //Constructors
-    Fraction(){
-        this->integer = this->numerator = 0;
-        this->denomenator = 1;
-        cout << "Default Constructor:\t" << this << endl;
-    }
-    Fraction(int integer){
-        this->integer = integer;
-        this->numerator = 0;
-        this->denomenator = 1;
-        cout << "1 arg. Constructor:\t" << this << endl;
-    }
-    Fraction(int numerator, int denumerator){
-        this->integer = 0;
-        this->numerator = numerator;
-        this->denomenator = denumerator;
-        cout << "2 arg. Constructor:\t" << this << endl;
-    }
-    Fraction(int integer , int numerator, int denumerator){
-        this->set_integer(integer);
-        this->set_numenator(numerator);
-        this->set_denomenator(denumerator);
-        cout << "3 arg. Constructor:\t" << this << endl;
-    }
-    Fraction(const Fraction& other){
-        this->integer = other.integer;
-        this->numerator = other.numerator;
-        this->denomenator = other.denomenator;
-        cout << "CopyConstructor:\t" << this << endl;
-    }
-    ~Fraction(){
-        cout << "Destrictor:\t" << this << endl;
-    }
-    //Operators:
-    Fraction& operator=(const Fraction& other){
-        this->integer = other.integer;
-        this->numerator = other.numerator;
-        this->denomenator = other.denomenator;
-        cout << "CopyAssignment:\t" << this << endl;
-        return *this;
-    }
-    Fraction& operator*=(const Fraction& other){
-        return *this = *this * other;
-    }
-    Fraction& operator/=(const Fraction other){
-        return *this = *this / other;
-    }
-    //increment/decrement
-    Fraction& operator++(){//Prefix increment
-        integer++;
-        return *this;
-    }
-    Fraction operator++(int){//Prefix increment
-        Fraction old = *this;
-        integer++;
-        return old;
-    }
-    Fraction& operator--(){
-        integer--;
-        return *this;
-    }
-    //Type cast operator
-    explicit operator int(){
-        return this->to_proper().integer;
-    }
-    explicit operator double(){
-        to_proper();
-       return integer +(double)numerator / denomenator;
-    }
-    operator HouseNumber(){
+    for(rest = 1;rest;more = less,less = rest)rest = more%less;
+    int GCD = more; //GCD Greatest Common Dividor(Наибольший общий делитель)
+    numerator /=GCD;
+    denomenator /=GCD;
+    return *this;
+}
+//Constructors
+Fraction::Fraction(){
+    this->integer = this->numerator = 0;
+    this->denomenator = 1;
+    cout << "Default Constructor:\t" << this << endl;
+}
+Fraction::Fraction(int integer){
+    this->integer = integer;
+    this->numerator = 0;
+    this->denomenator = 1;
+    cout << "1 arg. Constructor:\t" << this << endl;
+}
+Fraction::Fraction(int numerator, int denumerator){
+    this->integer = 0;
+    this->numerator = numerator;
+    this->denomenator = denumerator;
+    cout << "2 arg. Constructor:\t" << this << endl;
+}
+Fraction::Fraction(int integer , int numerator, int denumerator){
+    this->set_integer(integer);
+    this->set_numenator(numerator);
+    this->set_denomenator(denumerator);
+    cout << "3 arg. Constructor:\t" << this << endl;
+}
+Fraction::Fraction(const Fraction& other){
+    this->integer = other.integer;
+    this->numerator = other.numerator;
+    this->denomenator = other.denomenator;
+    cout << "CopyConstructor:\t" << this << endl;
+}
+Fraction::~Fraction(){
+    cout << "Destrictor:\t" << this << endl;
+}
+//Operators:
+Fraction& Fraction::operator=(const Fraction& other){
+    this->integer = other.integer;
+    this->numerator = other.numerator;
+    this->denomenator = other.denomenator;
+    cout << "CopyAssignment:\t" << this << endl;
+    return *this;
+}
+Fraction& Fraction::operator*=(const Fraction& other){
+    return *this = *this * other;
+}
+Fraction& Fraction::operator/=(const Fraction other){
+    return *this = *this / other;
+}
+//increment/decrement
+Fraction& Fraction::operator++(){//Prefix increment
+    integer++;
+    return *this;
+}
+Fraction Fraction::operator++(int){//Prefix increment
+    Fraction old = *this;
+    integer++;
+    return old;
+}
+Fraction& Fraction::operator--(){
+    integer--;
+    return *this;
+}
+//Type cast operator
+Fraction::operator int(){
+    return this->to_proper().integer;
+}
+Fraction::operator double(){
+    to_proper();
+    return integer +(double)numerator / denomenator;
+}
+//operator Fraction::HouseNumber(){
 
-    }
-};
+//}
+// class definition end
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //#define CONSRUCTORS_CHECK
 //#define ASSIGNMENT_CHECK
 //#define ARITHMETICKS_CHECK
